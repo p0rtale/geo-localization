83b6068eaf28a2a08c1aa798a2f0a2b4a5585bd4
p0rtale <yupetryaev@mail.ru>
2024-12-22 04:50:17 +0300

    add Im2GPS dataset

--- src/datasets/im2gps_dataset.py
+++ src/datasets/im2gps_dataset.py
@@ -14,46 +14,32 @@ from src.utils.io_utils import ROOT_PATH
 logger = logging.getLogger(__name__)
 
 URL_LINKS = {
-    "mp16-images": "http://www.cis.jhu.edu/~shraman/TransLocator/Datasets/mp16_rgb_images.tgz",
-    "mp16-annotations": "http://www.cis.jhu.edu/~shraman/TransLocator/Annotations.zip",
+    "im2gps3k-images": "http://www.cis.jhu.edu/~shraman/TransLocator/Datasets/im2gps3k_rgb_images.tar.gz",
+    "im2gps3k-annotations": "https://raw.githubusercontent.com/TIBHannover/GeoEstimation/original_tf/meta/im2gps3k_places365.csv",
 }
 
 
-class MediaEvalDataset(BaseDataset):
-    def __init__(self, part, split=0.99, data_dir=None, *args, **kwargs):
-        assert part in ("train", "val")
-        self.part = part
-        self.split = split
-
+class Im2GPSDataset(BaseDataset):
+    def __init__(self, data_dir=None, *args, **kwargs):
         if data_dir is None:
-            data_dir = ROOT_PATH / "data" / "datasets" / "mp16"
+            data_dir = ROOT_PATH / "data" / "datasets" / "im2gps3k"
             data_dir.mkdir(exist_ok=True, parents=True)
         self._data_dir = ROOT_PATH / data_dir
-        self._images_dir = (
-            self._data_dir / "r13_local_data" / "mp16" / "mp16_rgb_images"
-        )
+        self._images_dir = self._data_dir / "im2gps3k_rgb_images"
         self._annotations_path = self._data_dir / "annotations.csv"
 
-        logger.info("Building MP16 index...")
+        logger.info("Building Im2GPS3k index...")
         index = self._get_or_load_index()
-        logger.info("MP16 dataset is ready")
+        logger.info("Im2GPS3k dataset is ready")
 
         super().__init__(index, *args, **kwargs)
 
     def _load_annotations(self):
-        arch_path = self._data_dir / "annotations.zip"
-        self._load_data(URL_LINKS["mp16-annotations"], str(arch_path))
-        shutil.unpack_archive(arch_path, self._data_dir)
-        shutil.move(
-            str(self._data_dir / "Annotations" / "mp16_places365.csv"),
-            str(self._annotations_path),
-        )
-        os.remove(str(arch_path))
-        shutil.rmtree(str(self._data_dir / "Annotations"))
+        self._load_data(URL_LINKS["im2gps3k-annotations"], str(self._annotations_path))
 
     def _load_images(self):
-        arch_path = self._data_dir / "images.tgz"
-        self._load_data(URL_LINKS["mp16-images"], str(arch_path))
+        arch_path = self._data_dir / "images.tar.gz"
+        self._load_data(URL_LINKS["im2gps3k-images"], str(arch_path))
         shutil.unpack_archive(arch_path, self._data_dir)
         os.remove(str(arch_path))
 
@@ -86,21 +72,13 @@ class MediaEvalDataset(BaseDataset):
         ]
 
         annotations_list = annotations_df.to_dict(orient="records")
-
-        random.seed(42)
-        random.shuffle(annotations_list)
-
-        split_size = int(self.split * len(annotations_list))
-        if self.part == "train":
-            return annotations_list[:split_size]
-
-        return annotations_list[split_size:]
+        return annotations_list
 
     def _get_or_load_index(self):
         if not self._images_dir.exists():
             self._load_images()
 
-        index_path = self._data_dir / f"{self.part}_index.json"
+        index_path = self._data_dir / "index.json"
         if index_path.exists():
             with index_path.open() as file:
                 index = json.load(file)
